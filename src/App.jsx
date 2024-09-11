588e5841120f040c8df65367b573e120f66992db
unknown <sudeshmd1997@gmail.com>
2024-09-11 17:09:44 +0530

    pagination added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
-// src/App.js
-import useModal from './CustomHook/useModal';
-import Modal from './CustomHook/Modal';
+import React from 'react'
 
 const App = () => {
-  const { isOpen, openModal, closeModal } = useModal();
-
   return (
     <div>
-      <h1>My App</h1>
-      <button onClick={openModal}>Open Modal</button>
-      <Modal isOpen={isOpen} onClose={closeModal}>
-        <h2>Modal Content</h2>
-        <p>This is some content inside the modal.</p>
-      </Modal>
+      hello
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
